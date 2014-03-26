1a41cd66895ec2123ae46c288230b9b095c95449
Andrew Wroblicky <andrew.wroblicky@solutionset.com>
2014-03-25 18:55:28 -0700

    adding base junit test classes for chess pieces

---
+++ src/test/joust/core/chesspiece/ChessPieceTest.java
@@ -0,0 +1,89 @@
+package test.joust.core.chesspiece;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChessPieceTest {
+
+	@Test
+	public void testGetLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAlive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMyType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMySymbol() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRelativeRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPossibleMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDefenseMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanReach() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanDefend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckAvailability() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/joust/core/chesspiece/PawnTest.java
@@ -0,0 +1,134 @@
+package test.joust.core.chesspiece;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PawnTest {
+
+	@Test
+	public void testGetLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAlive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMyType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMySymbol() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRelativeRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPossibleMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDefenseMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanReach() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanDefend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPawn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPawnLocationStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetmovedYet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMovedTwice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRound() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetmovedYet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMovedTwice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRound() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsChessPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckAvailability() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
