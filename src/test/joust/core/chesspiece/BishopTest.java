f5e8cebc3796034e51f12964012c55f6fcd4d4ae
Andrew Wroblicky <andrew.wroblicky@solutionset.com>
2014-03-25 18:25:41 -0700

    removed .classpath from project as it is environment specific

---
+++ src/test/joust/core/chesspiece/BishopTest.java
@@ -0,0 +1,104 @@
+package test.joust.core.chesspiece;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BishopTest {
+
+	@Test
+	public void testGetLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAlive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetmyType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetmySymbol() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRelRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPossibleMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDefenseMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanReach() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanDefend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBishop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNowDead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsChessPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckAvailability() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
