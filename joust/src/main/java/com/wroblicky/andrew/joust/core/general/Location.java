a323b633fe7a263814847d140cb770e31a113ebf
Andrew Wroblicky <andrew.wroblicky@solutionset.com>
2014-04-30 13:59:03 -0700

    cleaned up Location.java class

--- joust/src/main/java/com/wroblicky/andrew/joust/core/general/Location.java
+++ joust/src/main/java/com/wroblicky/andrew/joust/core/general/Location.java
@@ -10,34 +10,6 @@ public class Location {
 	private String myRank;
 	private ChessPiece myChessPiece;
 	
-	//Doesn't work needs to be rewritten
-	public Location(int x, int y) {
-		//System.out.println("x: " + x);
-		//System.out.println("y: " + y);
-		myY = y;
-		myX = x;
-		String stringX = null;
-		if (x == 1) {
-			stringX = "a";
-		} else if (x == 2) {
-			stringX = "b";
-		} else if (x == 3) {
-			stringX = "c";
-		} else if (x == 4) {
-			stringX = "d";
-		} else if (x == 5) {
-			stringX = "e";
-		} else if (x == 6) {
-			stringX = "f";
-		} else if (x == 7) {
-			stringX = "g";
-		} else if (x == 8) {
-			stringX = "h";
-		}
-		myAlgebraicLocation = stringX + myY;
-		//System.out.println("myAlgebra: " + myAlgebraicLocation);
-	}
-	
 	public Location(String s) {
 		myAlgebraicLocation = s;
 		myFile = s.substring(0, 1);
@@ -62,47 +34,28 @@ public class Location {
 		}
 	}
 	
-	public Location (String algebraicLocation, ChessPiece chessPiece) {
-		myAlgebraicLocation = algebraicLocation;
-		String x = algebraicLocation.substring(0, 1);
-		myFile = x;
-		String y = algebraicLocation.substring(1, 2);
-		myRank = y;
-		//System.out.println("y: " + y);
-		int y2 = Integer.parseInt(y);
-		if (x.equals("a")) {
-			myX = 1;
-		} else if (x.equals("b")) {
-			myX = 2;
-		} else if (x.equals("c")) {
-			myX = 3;
-		} else if (x.equals("d")) {
-			myX = 4;
-		} else if (x.equals("e")) {
-			myX = 5;
-		} else if (x.equals("f")) {
-			myX = 6;
-		} else if (x.equals("g")) {
-			myX = 7;
-		} else if (x.equals("h")) {
-			myX = 8;
-		}
-		//System.out.println("location constructor x: " + x);
-		//System.out.println("location constructor myX: " + myX);
-		myY = y2;
-		myChessPiece = chessPiece;
-	}
-	
 	public int getXCoordinate() {
 		return myX;
+	}
+	
+	public void setXCoordinate(int x) {
+		myX = x;
 	}
 	
 	public int getYCoordinate() {
 		return myY;
 	}
 	
+	public void setYCoordinate(int y) {
+		myY = y;
+	}
+	
 	public String getAlgebraicLocation() {
 		return myAlgebraicLocation;
+	}
+	
+	public void setMyAlgebraicLocation(String al) {
+		myAlgebraicLocation = al;
 	}
 	
 	public String getFile() {
@@ -113,17 +66,12 @@ public class Location {
 		return myRank;
 	}
 	
-	//All setters need to be fixed. Not priority at moment though
-	public void setXCoordinate(int x) {
-		myX = x;
+	public ChessPiece getChessPiece() {
+		return myChessPiece;
 	}
 	
-	public void setYCoordinate(int y) {
-		myY = y;
-	}
-	
-	public void setMyAlgebraicLocation(String al) {
-		myAlgebraicLocation = al;
+	public void setChessPiece(ChessPiece chessPiece) {
+		this.myChessPiece = chessPiece;
 	}
 	
 	public boolean equals(Location l) {
@@ -132,16 +80,6 @@ public class Location {
 			toReturn = true;
 		}
 		return toReturn;
-	}
-	
-	// use chess board getLocation instead
-	@Deprecated
-	public Location move(int up, int left, int down, int right) {
-		Location l = null;
-		if((myX - left + right >= 1 || myX - left + right <= 8 ) && (myY + up - down >= 1 || myY + up - down <= 8)) {
-			l = new Location(myX - left + right, myY + up - down);
-		}
-		return l;
 	}
 	
 	public static String convert(int x, int y) {
@@ -164,14 +102,6 @@ public class Location {
 			stringX = "h";
 		}
 		return stringX + y;
-	}
-	
-	public ChessPiece getChessPiece() {
-		return myChessPiece;
-	}
-	
-	public void setChessPiece(ChessPiece chessPiece) {
-		this.myChessPiece = chessPiece;
 	}
 	
 	public String toString() {
