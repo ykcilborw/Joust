325d0d0eb99c67378137a9c6965e0a271d535e5b
Andrew Wroblicky <andrew.wroblicky@solutionset.com>
2014-06-06 15:13:45 -0700

    renamed GameManager to PGNViewer

--- joust/src/main/java/com/wroblicky/andrew/joust/core/game/PGNViewer.java
+++ joust/src/main/java/com/wroblicky/andrew/joust/core/game/PGNViewer.java
@@ -1,16 +1,20 @@
 package com.wroblicky.andrew.joust.core.game;
 
 
-public interface GameManager {
+public final class PGNViewer {
 	
 	/**
 	 * Updates the game state after another turn is played
 	 */
-	public Game playNextTurn();
+	public Game playNextTurn() {
+		return null;
+	}
 	
 	/**
 	 * Brings back the game state to what it was before the last turn was played
 	 */
-	public Game undoCurrentTurn();
+	public Game undoCurrentTurn() {
+		return null;
+	}
 
 }
