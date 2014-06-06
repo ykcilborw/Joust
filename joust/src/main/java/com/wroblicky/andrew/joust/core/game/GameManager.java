798481938ae1b4f7d930baf02f20a9ba848a539c
Andrew Wroblicky <andrew.wroblicky@solutionset.com>
2014-06-06 14:28:41 -0700

    no need to distinguish between Game and GameState

--- joust/src/main/java/com/wroblicky/andrew/joust/core/game/GameManager.java
+++ joust/src/main/java/com/wroblicky/andrew/joust/core/game/GameManager.java
@@ -6,15 +6,11 @@ public interface GameManager {
 	/**
 	 * Updates the game state after another turn is played
 	 */
-	public void playNextTurn();
+	public Game playNextTurn();
 	
 	/**
 	 * Brings back the game state to what it was before the last turn was played
 	 */
-	public void undoCurrentTurn();
-	
-	/**
-	 * Returns the game object it's managing
-	 */
-	public Game getGame();
+	public Game undoCurrentTurn();
+
 }
