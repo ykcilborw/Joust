348c234da84e344f836c9930fec8161493bf9c69
Andrew Wroblicky <andrew.wroblicky@solutionset.com>
2014-07-11 16:46:03 -0700

    adding the DisplayConstants class

---
+++ joust/src/main/java/com/wroblicky/andrew/joust/display/DisplayConstants.java
@@ -0,0 +1,9 @@
+package com.wroblicky.andrew.joust.display;
+
+public final class DisplayConstants {
+	
+	public static final String INTERACTIVE_MODE = "interactive";
+	
+	public static final String NONINTERACTIVE_MODE = "non-interactive";
+
+}
