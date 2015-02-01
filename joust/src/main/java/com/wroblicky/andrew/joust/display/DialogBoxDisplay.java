e6478a5fede8082b2bf8b071ff5459019142a2e3
Andrew Wroblicky <ykcilborw@yahoo.com>
2015-02-01 02:40:55 -0800

    Updated gui buttons to not use symbols like < in favor of human words like 'next'. Also removed trailing whitespace from DialogBoxDisplay class

--- joust/src/main/java/com/wroblicky/andrew/joust/display/DialogBoxDisplay.java
+++ joust/src/main/java/com/wroblicky/andrew/joust/display/DialogBoxDisplay.java
@@ -2,7 +2,6 @@ package com.wroblicky.andrew.joust.display;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -36,151 +35,166 @@ import com.wroblicky.andrew.joust.game.move.Turn;
 
 /**
  * Manages the ui for displaying a chess game as a dialog box
- * 
+ *
  * @author Andrew Wroblicky
  *
  */
 @SuppressWarnings("serial")
 public class DialogBoxDisplay extends JFrame implements ActionListener {
-	
-	private JLayeredPane layeredPane;
-	private JPanel chessBoardPanel;
-	private PGNViewer pgnViewer;
+
+	private final JLayeredPane layeredPane;
+	private final JPanel chessBoardPanel;
+	private final PGNViewer pgnViewer;
 
 	private DialogBoxDisplay(Game game, PGNViewer pgnViewer) {
 		this.pgnViewer = pgnViewer;
 		Dimension boardSize = new Dimension(600, 600);
-		
+
 		layeredPane = new JLayeredPane();
 		getContentPane().add(layeredPane);
 		layeredPane.setPreferredSize(boardSize);
-		
+
 		addMenuBar();
-		
+
 		// setup chess board
 		chessBoardPanel = new JPanel();
 		layeredPane.add(chessBoardPanel, JLayeredPane.DEFAULT_LAYER);
-		chessBoardPanel.setLayout( new GridLayout(9, 8) );
-		chessBoardPanel.setPreferredSize( boardSize );
+		chessBoardPanel.setLayout(new GridLayout(9, 8));
+		chessBoardPanel.setPreferredSize(boardSize);
 		chessBoardPanel.setBounds(0, 0, boardSize.width, boardSize.height);
 		for (int i = 0; i < 64; i++) {
-			JPanel square = new JPanel( new BorderLayout() );
-			chessBoardPanel.add( square );
+			JPanel square = new JPanel(new BorderLayout());
+			chessBoardPanel.add(square);
 			int row = (i / 8) % 2;
-			if (row == 0)
-				square.setBackground( i % 2 == 0 ? Color.black : Color.red );
-			else
-				square.setBackground( i % 2 == 0 ? Color.red : Color.black );
+			if (row == 0) {
+				square.setBackground(i % 2 == 0 ? Color.black : Color.red);
+			} else {
+				square.setBackground(i % 2 == 0 ? Color.red : Color.black);
+			}
 		}
-		
+
 		ChessBoardIterator chessBoardIterator = game.getBoard().iterator();
 		while (chessBoardIterator.hasNext()) {
 			Location current = chessBoardIterator.next();
 			ChessPiece chessPiece = current.getChessPiece();
 			addChessPiece(current, chessPiece);
 		}
-		
-		// fast forward button
-		JButton fastForwardButton = new JButton(">>");
+
+		// end button
+		JButton fastForwardButton = new JButton("End");
 		fastForwardButton.setVerticalTextPosition(AbstractButton.CENTER);
-	    fastForwardButton.setHorizontalTextPosition(AbstractButton.CENTER);
-	    fastForwardButton.setMnemonic(KeyEvent.VK_D);
-	    fastForwardButton.setActionCommand("fastForwardButton");
-	    fastForwardButton.addActionListener(this);
+		fastForwardButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		fastForwardButton.setMnemonic(KeyEvent.VK_D);
+		fastForwardButton.setActionCommand("fastForwardButton");
+		fastForwardButton.addActionListener(this);
 
 		// next button
-		JButton nextButton = new JButton(">");
-	    nextButton.setVerticalTextPosition(AbstractButton.CENTER);
-	    nextButton.setHorizontalTextPosition(AbstractButton.CENTER);
-	    nextButton.setMnemonic(KeyEvent.VK_D);
-	    nextButton.setActionCommand("next");
-	    nextButton.addActionListener(this);
-	    
-	    // back button
-	    JButton backButton = new JButton("<");
-	    backButton.setVerticalTextPosition(AbstractButton.CENTER);
-	    backButton.setHorizontalTextPosition(AbstractButton.CENTER);
-	    backButton.setMnemonic(KeyEvent.VK_D);
-	    backButton.setActionCommand("back");
-	    backButton.addActionListener(this);
-	    
-	    // rewind button
-	    JButton rewindButton = new JButton("<<");
-	    rewindButton.setVerticalTextPosition(AbstractButton.CENTER);
-	    rewindButton.setHorizontalTextPosition(AbstractButton.CENTER);
-	    rewindButton.setMnemonic(KeyEvent.VK_D);
-	    rewindButton.setActionCommand("rewind");
-	    rewindButton.addActionListener(this);
-	    
-	    chessBoardPanel.add(rewindButton);
-	    chessBoardPanel.add(backButton);
+		JButton nextButton = new JButton("Next");
+		nextButton.setVerticalTextPosition(AbstractButton.CENTER);
+		nextButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		nextButton.setMnemonic(KeyEvent.VK_D);
+		nextButton.setActionCommand("next");
+		nextButton.addActionListener(this);
+
+		// back button
+		JButton backButton = new JButton("Back");
+		backButton.setVerticalTextPosition(AbstractButton.CENTER);
+		backButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		backButton.setMnemonic(KeyEvent.VK_D);
+		backButton.setActionCommand("back");
+		backButton.addActionListener(this);
+
+		// start button
+		JButton rewindButton = new JButton("Start");
+		rewindButton.setVerticalTextPosition(AbstractButton.CENTER);
+		rewindButton.setHorizontalTextPosition(AbstractButton.CENTER);
+		rewindButton.setMnemonic(KeyEvent.VK_D);
+		rewindButton.setActionCommand("rewind");
+		rewindButton.addActionListener(this);
+
+		chessBoardPanel.add(rewindButton);
+		chessBoardPanel.add(backButton);
 		chessBoardPanel.add(nextButton);
 		chessBoardPanel.add(fastForwardButton);
 	}
-	
+
 	// http://zetcode.com/tutorials/javaswingtutorial/menusandtoolbars/
 	private void addMenuBar() {
 		JMenuBar menubar = new JMenuBar();
 		JMenu file = new JMenu("File");
-        file.setMnemonic(KeyEvent.VK_F);
-        
-        JMenuItem eMenuItem = new JMenuItem("Exit");
-        eMenuItem.setMnemonic(KeyEvent.VK_E);
-        eMenuItem.setToolTipText("Exit application");
-        
-        file.add(eMenuItem);
-        menubar.add(file);
-        setJMenuBar(menubar);
+		file.setMnemonic(KeyEvent.VK_F);
+
+		JMenuItem eMenuItem = new JMenuItem("Exit");
+		eMenuItem.setMnemonic(KeyEvent.VK_E);
+		eMenuItem.setToolTipText("Exit application");
+
+		file.add(eMenuItem);
+		menubar.add(file);
+		setJMenuBar(menubar);
 	}
-	
+
 	private void addChessPiece(Location location, ChessPiece chessPiece) {
 		if (chessPiece != null) {
 			switch (chessPiece.getChessPieceAllegianceType()) {
-				case BLACK_PAWN:	addChessPiece(location, "SnippingPawn.JPG");
-									break;
-				case WHITE_PAWN:	addChessPiece(location, "SnippingWhitePawn.JPG");
-									break;
-				case BLACK_ROOK:	addChessPiece(location, "SnippingRook.JPG");
-									break;
-				case WHITE_ROOK:	addChessPiece(location, "SnippingWhiteRook.JPG");
-									break;
-				case BLACK_KNIGHT:	addChessPiece(location, "SnippingHorse.JPG");
-									break;
-				case WHITE_KNIGHT:	addChessPiece(location, "SnippingWhiteHorse.JPG");
-									break;
-				case BLACK_BISHOP:	addChessPiece(location, "SnippingBishop.JPG");
-									break;
-				case WHITE_BISHOP:	addChessPiece(location, "SnippingWhiteBishop.JPG");
-									break;
-				case BLACK_QUEEN:	addChessPiece(location, "SnippingQueen.JPG");
-									break;
-				case WHITE_QUEEN:	addChessPiece(location, "SnippingWhiteQueen.JPG");
-									break;
-				case BLACK_KING:	addChessPiece(location, "SnippingKing.JPG");
-									break;
-				case WHITE_KING:	addChessPiece(location, "SnippingWhiteKing.JPG");
-									break;
+			case BLACK_PAWN:
+				addChessPiece(location, "SnippingPawn.JPG");
+				break;
+			case WHITE_PAWN:
+				addChessPiece(location, "SnippingWhitePawn.JPG");
+				break;
+			case BLACK_ROOK:
+				addChessPiece(location, "SnippingRook.JPG");
+				break;
+			case WHITE_ROOK:
+				addChessPiece(location, "SnippingWhiteRook.JPG");
+				break;
+			case BLACK_KNIGHT:
+				addChessPiece(location, "SnippingHorse.JPG");
+				break;
+			case WHITE_KNIGHT:
+				addChessPiece(location, "SnippingWhiteHorse.JPG");
+				break;
+			case BLACK_BISHOP:
+				addChessPiece(location, "SnippingBishop.JPG");
+				break;
+			case WHITE_BISHOP:
+				addChessPiece(location, "SnippingWhiteBishop.JPG");
+				break;
+			case BLACK_QUEEN:
+				addChessPiece(location, "SnippingQueen.JPG");
+				break;
+			case WHITE_QUEEN:
+				addChessPiece(location, "SnippingWhiteQueen.JPG");
+				break;
+			case BLACK_KING:
+				addChessPiece(location, "SnippingKing.JPG");
+				break;
+			case WHITE_KING:
+				addChessPiece(location, "SnippingWhiteKing.JPG");
+				break;
 			}
 		}
 	}
-	
+
 	private void addChessPiece(Location location, String imageName) {
 		try {
 			BufferedImage myPicture = ImageIO.read(new File(imageName));
-			JLabel piece = new JLabel( new ImageIcon(myPicture) );
-			JPanel panel = (JPanel)chessBoardPanel.getComponent(location.getComponentNumber());
+			JLabel piece = new JLabel(new ImageIcon(myPicture));
+			JPanel panel = (JPanel) chessBoardPanel.getComponent(location
+					.getComponentNumber());
 			panel.add(piece);
 		} catch (IOException e) {
 			Util.print("io: " + e);
 			e.printStackTrace();
 		}
 	}
-	
+
+	@Override
 	public void actionPerformed(ActionEvent e) {
 		String which = e.getActionCommand();
-		if(which.equals("next")) {
+		if (which.equals("next")) {
 			handleNext();
-		} else if(which.equals("back")) {
+		} else if (which.equals("back")) {
 			handleBack();
 		} else if (which.equals("fastforward")) {
 			// TODO
@@ -188,7 +202,7 @@ public class DialogBoxDisplay extends JFrame implements ActionListener {
 			// TODO
 		}
 	}
-	
+
 	private void handleNext() {
 		Game game = pgnViewer.playNextTurn();
 		Turn turn = game.getCurrentTurn();
@@ -203,7 +217,7 @@ public class DialogBoxDisplay extends JFrame implements ActionListener {
 			}
 		}
 	}
-	
+
 	private void handleBack() {
 		Game game = pgnViewer.getGame();
 		Turn turn = game.getCurrentTurn();
@@ -219,31 +233,31 @@ public class DialogBoxDisplay extends JFrame implements ActionListener {
 			}
 		}
 	}
-	
+
 	private void moveChessPiece(Location initial, Location destination) {
 		// prepare sourcePanel
-		JPanel sourcePanel = (JPanel) chessBoardPanel.getComponent(
-				initial.getComponentNumber());
+		JPanel sourcePanel = (JPanel) chessBoardPanel.getComponent(initial
+				.getComponentNumber());
 		JLabel chessPiece = (JLabel) sourcePanel.getComponentAt(1, 0);
 		chessPiece.setVisible(false);
-		
+
 		// prepare destinationPanel
-		JPanel destinationPanel =  (JPanel) chessBoardPanel.getComponent(
-				destination.getComponentNumber());
+		JPanel destinationPanel = (JPanel) chessBoardPanel
+				.getComponent(destination.getComponentNumber());
 		destinationPanel.add(chessPiece);
 		chessPiece.setVisible(true);
 	}
-	
+
 	private void removeChessPiece(Location location) {
-		JPanel sourcePanel = (JPanel) chessBoardPanel.getComponent(
-				location.getComponentNumber());
+		JPanel sourcePanel = (JPanel) chessBoardPanel.getComponent(location
+				.getComponentNumber());
 		JLabel chessPiece = (JLabel) sourcePanel.getComponentAt(1, 0);
 		chessPiece.setVisible(false);
 		sourcePanel.remove(chessPiece);
 		sourcePanel.revalidate();
 		sourcePanel.repaint();
 	}
-	
+
 	public static void start(Game game, PGNViewer pgnViewer) {
 		JFrame frame = new DialogBoxDisplay(game, pgnViewer);
 		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
